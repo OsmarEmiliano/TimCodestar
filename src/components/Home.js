c6c0dde86c12a2bd379d626ced1dbd6589b3d787
brayansadi55 <152029787+brayansadi55@users.noreply.github.com>
2024-02-01 23:13:16 -0600

    Primera vercion

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import RegisterButton from './RegisterButton';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Bienvenido a mi portafolio</h1>
+      {/* Contenido de la sección de inicio */}
+      <RegisterButton />
+    </div>
+  );
+};
+
+export default Home;
