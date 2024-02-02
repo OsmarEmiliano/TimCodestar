403dac10ae0bfee35ba0f745407cd8b52a98097a
brayansadi55 <152029787+brayansadi55@users.noreply.github.com>
2024-02-02 14:35:40 -0600

    Modificacion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Home from './components/Home';
-import About from './components/About';
-import Projects from './components/Projects';
-import Contact from './components/Contact';
+import './css/style.css';
 
 const App = () => {
-  return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
-    </Router>
-  );
-};
+    return (
+        <div>
+            {/* Contenido de tu aplicación React */}
+        </div>
+    );
+}
 
 export default App;
+
